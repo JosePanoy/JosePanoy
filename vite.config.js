426c65f87a7dad1c579f6f461eae10d67fd1ae46
JosePanoy <calderon.janrasheedcit2011@gmail.com>
2024-06-13 14:24:49 +0800

    chnage vit.config 2:25 pm

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Custom resolver plugin to handle "react-dom/client"
-const customResolver = {
-  name: 'custom-resolver',
-  resolveId(source) {
-    if (source === 'react-dom/client') {
-      return {
-        id: 'react-dom',
-        external: true,
-      };
-    }
-    return null;
-  },
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    customResolver // Add the custom resolver plugin
   ],
+  resolve: {
+    alias: {
+      'react-dom/client': 'react-dom',
+    },
+  },
 });
