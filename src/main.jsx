897e645307f16dfbd4ba75af9e892d0e3f370a30
JosePanoy <calderon.janrasheedcit2011@gmail.com>
2024-06-13 14:31:23 +0800

    changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')).render( // Corrected usage
+createRoot(document.getElementById('root')).render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
