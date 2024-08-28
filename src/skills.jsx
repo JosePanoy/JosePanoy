a151f62457ee00c477656ce331e92abd1511ce7b
Jan Rasheed Calderon <calderon.janrasheedcit2011@gmail.com>
2024-08-28 16:53:33 +0800

    added skills

--- src/skills.jsx
+++ src/skills.jsx
@@ -12,6 +12,9 @@ import CSSLogo from '../src/assets/img/logo/css.png';
 import PhotoshopLogo from '../src/assets/img/logo/photoshop.png';
 import IllustratorLogo from '../src/assets/img/logo/illustrator.png';
 import GithubLogo from '../src/assets/img/logo/github.png';
+import NodeLogo from '../src/assets/img/logo/nodejs.png';
+import ExpressLogo from '../src/assets/img/logo/expressjs.png';
+import MongoLogo from '../src/assets/img/logo/mongodb.png';
 
 const topRowSkills = [
   { img: PHPLogo, name: 'PHP' },
@@ -19,12 +22,15 @@ const topRowSkills = [
   { img: JavascriptLogo, name: 'JavaScript' },
   { img: ReactLogo, name: 'React' },
   { img: GithubLogo, name: 'GitHub' },
+  { img: MongoLogo, name: 'MongoDB' },
 ];
 
 const bottomRowSkills = [
   { img: HTMLLogo, name: 'HTML' },
   { img: CSSLogo, name: 'CSS' },
   { img: BootstrapLogo, name: 'Bootstrap' },
+  { img: NodeLogo, name: 'Node JS' },
+  { img: ExpressLogo, name: 'Express JS' },
   { img: PhotoshopLogo, name: 'Photoshop' },
   { img: IllustratorLogo, name: 'Illustrator' },
 ];
