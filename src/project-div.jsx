b29be2fd2cd8127b0a105c18d9a6e5951d69b6d8
Jan Rasheed Calderon <calderon.janrasheedcit2011@gmail.com>
2024-09-05 11:54:29 +0800

    added new project

--- src/project-div.jsx
+++ src/project-div.jsx
@@ -8,10 +8,11 @@ import TodolistPic from './assets/img/todolist.gif';
 import CalculatorPic from './assets/img/calculator.gif';
 import RecipeFinder from './assets/img/cooking.gif';
 import MovieFind from './assets/img/movie.gif';
-import CamPic from './assets/img/cam.png'
-import PFHPic from './assets/img/pfh.png'
-import ShoeStorePic from './assets/img/shoestore.gif'
-import ServicePage from './assets/img/servicepage.gif'
+import CamPic from './assets/img/cam.png';
+import PFHPic from './assets/img/pfh.png';
+import ShoeStorePic from './assets/img/shoestore.gif';
+import ServicePage from './assets/img/servicepage.gif';
+import PanoySocial from './assets/mp4/panoysocial.mp4'; // Video Import
 import { Link } from 'react-router-dom';
 
 function SideProjects() {
@@ -20,12 +21,13 @@ function SideProjects() {
         threshold: 0.1,
     });
 
-    const fadeInFromBottom = (delay) => useSpring({
-        opacity: inView ? 1 : 0,
-        transform: inView ? 'translateY(0)' : 'translateY(50px)',
-        config: { duration: 700 },
-        delay: delay,
-    });
+    const fadeInFromBottom = (delay) =>
+        useSpring({
+            opacity: inView ? 1 : 0,
+            transform: inView ? 'translateY(0)' : 'translateY(50px)',
+            config: { duration: 700 },
+            delay: delay,
+        });
 
     const fadeInFromBottom1 = fadeInFromBottom(0);
     const fadeInFromBottom2 = fadeInFromBottom(200);
@@ -43,7 +45,7 @@ function SideProjects() {
                 className="project-item"
                 style={{
                     ...fadeInFromBottom1,
-                    backgroundImage: `url(${MainPicture})`
+                    backgroundImage: `url(${MainPicture})`,
                 }}
                 data-title="WIT Online Library System (2024)"
             >
@@ -55,11 +57,39 @@ function SideProjects() {
                     </Link>
                 </div>
             </animated.div>
+
+            <animated.div
+                className="project-item"
+                style={fadeInFromBottom2}
+                data-title="Panoy Social"
+            >
+                <video
+                    className="project-video"
+                    src={PanoySocial}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                />
+                <div className="project-details">
+                    <h3 className="project-title">Panoy Social</h3>
+                    <p className="project-progress">Progress: 70%</p>
+                    <a
+                        href="http://panoysocial.vercel.app/"
+                        className="project-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </animated.div>
+
             <animated.div
                 className="project-item"
                 style={{
-                    ...fadeInFromBottom2,
-                    backgroundImage: `url(${TodolistPic})`
+                    ...fadeInFromBottom3,
+                    backgroundImage: `url(${TodolistPic})`,
                 }}
                 data-title="TodoList"
             >
@@ -71,11 +101,12 @@ function SideProjects() {
                     </Link>
                 </div>
             </animated.div>
+
             <animated.div
                 className="project-item"
                 style={{
-                    ...fadeInFromBottom3,
-                    backgroundImage: `url(${CalculatorPic})`
+                    ...fadeInFromBottom4,
+                    backgroundImage: `url(${CalculatorPic})`,
                 }}
                 data-title="Calculator"
             >
@@ -87,11 +118,12 @@ function SideProjects() {
                     </Link>
                 </div>
             </animated.div>
+
             <animated.div
                 className="project-item"
                 style={{
-                    ...fadeInFromBottom4,
-                    backgroundImage: `url(${RecipeFinder})`
+                    ...fadeInFromBottom5,
+                    backgroundImage: `url(${RecipeFinder})`,
                 }}
                 data-title="Recipe Finder"
             >
@@ -104,12 +136,11 @@ function SideProjects() {
                 </div>
             </animated.div>
 
-
             <animated.div
                 className="project-item"
                 style={{
-                    ...fadeInFromBottom5,
-                    backgroundImage: `url(${MovieFind})`
+                    ...fadeInFromBottom6,
+                    backgroundImage: `url(${MovieFind})`,
                 }}
                 data-title="Movie Find"
             >
@@ -123,75 +154,92 @@ function SideProjects() {
             </animated.div>
 
             <animated.div
-    className="project-item"
-    style={{
-        ...fadeInFromBottom6,
-        backgroundImage: `url(${CamPic})`
-    }}
-    data-title="Christian Anthony Photos"
->
-    <div className="project-details">
-        <h3 className="project-title">Christian Anthony Photos</h3>
-        <p className="project-progress">Progress: 70%</p>
-        <a href="http://christiananthony-photos.vercel.app/" className="project-link" target="_blank" rel="noopener noreferrer">
-            Visit <FaExternalLinkAlt />
-        </a>
-    </div>
-</animated.div>
+                className="project-item"
+                style={{
+                    ...fadeInFromBottom7,
+                    backgroundImage: `url(${CamPic})`,
+                }}
+                data-title="Christian Anthony Photos"
+            >
+                <div className="project-details">
+                    <h3 className="project-title">Christian Anthony Photos</h3>
+                    <p className="project-progress">Progress: 90%</p>
+                    <a
+                        href="http://christiananthony-photos.vercel.app/"
+                        className="project-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </animated.div>
 
-<animated.div
-    className="project-item"
-    style={{
-        ...fadeInFromBottom7,
-        backgroundImage: `url(${PFHPic})`
-    }}
-    data-title="Pro Footie Hub"
->
-    <div className="project-details">
-        <h3 className="project-title">Pro Footie Hub</h3>
-        <p className="project-progress">Progress: 50%</p>
-        <a href="http://pfh-seven.vercel.app/" className="project-link" target="_blank" rel="noopener noreferrer">
-            Visit <FaExternalLinkAlt />
-        </a>
-    </div>
-</animated.div>
+            <animated.div
+                className="project-item"
+                style={{
+                    ...fadeInFromBottom8,
+                    backgroundImage: `url(${PFHPic})`,
+                }}
+                data-title="Pro Footie Hub"
+            >
+                <div className="project-details">
+                    <h3 className="project-title">Pro Footie Hub</h3>
+                    <p className="project-progress">Progress: 50%</p>
+                    <a
+                        href="http://pfh-seven.vercel.app/"
+                        className="project-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </animated.div>
 
-<animated.div
-    className="project-item"
-    style={{
-        ...fadeInFromBottom7,
-        backgroundImage: `url(${ShoeStorePic})`
-    }}
-    data-title="Foot Flick"
->
-    <div className="project-details">
-        <h3 className="project-title">Foot Flick</h3>
-        <p className="project-progress">Progress: 60%</p>
-        <a href="http://footflick.vercel.app/" className="project-link" target="_blank" rel="noopener noreferrer">
-            Visit <FaExternalLinkAlt />
-        </a>
-    </div>
-</animated.div>
+            <animated.div
+                className="project-item"
+                style={{
+                    ...fadeInFromBottom9,
+                    backgroundImage: `url(${ShoeStorePic})`,
+                }}
+                data-title="Foot Flick"
+            >
+                <div className="project-details">
+                    <h3 className="project-title">Foot Flick</h3>
+                    <p className="project-progress">Progress: 60%</p>
+                    <a
+                        href="http://footflick.vercel.app/"
+                        className="project-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </animated.div>
 
-<animated.div
-    className="project-item"
-    style={{
-        ...fadeInFromBottom8,
-        backgroundImage: `url(${ServicePage})`
-    }}
-    data-title="Service Page"
->
-    <div className="project-details">
-        <h3 className="project-title">Service Page</h3>
-        <p className="project-progress">Progress: 100%</p>
-        <a href="https://servicepage-one.vercel.app/" className="project-link" target="_blank" rel="noopener noreferrer">
-            Visit <FaExternalLinkAlt />
-        </a>
-    </div>
-</animated.div>
-
-
-
+            <animated.div
+                className="project-item"
+                style={{
+                    ...fadeInFromBottom9,
+                    backgroundImage: `url(${ServicePage})`,
+                }}
+                data-title="Service Page"
+            >
+                <div className="project-details">
+                    <h3 className="project-title">Service Page</h3>
+                    <p className="project-progress">Progress: 100%</p>
+                    <a
+                        href="https://servicepage-one.vercel.app/"
+                        className="project-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </animated.div>
         </animated.div>
     );
 }
